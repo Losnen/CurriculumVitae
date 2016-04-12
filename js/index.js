520c882ae318fa6056d0d229a1d0c820f4c7fc5c
Samuel Ramos <alu0100815146@ull.edu.es>
2016-04-12 23:06:30 +0100

    Primer Commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// Elevator script included on the page, already.
+
+window.onload = function() {
+  var elevator = new Elevator({
+    element: document.querySelector('#boton'),
+    duration: 1000 // milliseconds
+  });
+}
